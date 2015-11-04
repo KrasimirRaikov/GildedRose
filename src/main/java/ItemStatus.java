7c4f92258ea1caccae767a28bbe0b750a02aa199
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-11-04 11:22:28 +0200

    refactored removing duplication in updateSellIn and upfateQuality

--- src/main/java/ItemStatus.java
+++ src/main/java/ItemStatus.java
@@ -24,28 +24,11 @@ public class ItemStatus {
     if (!item.getName().contains("Sulfuras")) {
       return item.getSellIn() - 1;
     } else {
-      return specialUpdateSellIn(item);
+      return item.getSellIn();
     }
   }
 
-  public int specialUpdateSellIn(Item item) {
-    if (item.getSellIn() < 0) {
-      if (!"Aged Brie".equals(item.getName())) {
-        if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-          if (item.getQuality() > 0 && (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))) {
-            return item.getQuality() - 1;
-          }
-        } else {
-          return 0;
-        }
-      } else {
-        if (item.getQuality() < 50) {
-          return item.getQuality() + 1;
-        }
-      }
-    }
-    return 101;
-  }
+
 
   public int updateQuality(Item item) {
     if ((!item.getName().contains("Aged Brie")) && !item.getName().contains("Backstage passes")) {
